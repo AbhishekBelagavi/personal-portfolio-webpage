17dd33a8135e80d5b7f6e4844dc0b7aa7834689d
Abhishek Belagavi <78343840+AbhishekBelagavi@users.noreply.github.com>
2024-06-16 14:22:26 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => (
     </div>
     <div className="text-center font-poppins font-normal text-dimWhite text-xs sm:text-sm pb-4">
       <p>
-        Made with 💜 by Rishi
+        Made with 💜 by Rohanithin
       </p>
     </div>
   </footer>
